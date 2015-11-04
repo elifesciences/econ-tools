4fa99187089ab8f1975820d0d22aa3682fb217ad
John <jhroot@users.noreply.github.com>
2015-11-04 16:51:23 +0000

    initial commit

---
+++ ppp-feeder.py
@@ -0,0 +1,73 @@
+from optparse import OptionParser
+from boto.s3.connection import S3Connection
+from boto.sqs.connection import SQSConnection
+import time
+import re
+
+
+def feed_ppp(bucket_name, queue_name, rate, prefix, key_filter):
+
+    print "feeding any keys in %s with prefix %s matching %s at a rate of 1 every %i seconds" % (
+        bucket_name, prefix, key_filter, rate)
+
+    queue = get_queue(queue_name)
+    keys = get_filtered_keys(bucket_name, prefix, key_filter)
+
+    for key in keys:
+        initiate_ppp(queue, key)
+        time.sleep(rate)
+
+
+def get_queue(queue_name):
+
+    sqs_conn = SQSConnection()
+    queue = sqs_conn.get_queue(queue_name)
+    return queue
+
+
+def get_filtered_keys(bucketname, prefix, key_filter):
+
+    conn = S3Connection()
+    bucket = conn.get_bucket(bucketname)
+    keys = bucket.list(prefix=prefix)
+
+    # the list method is a generator and efficiently handles paging over a large
+    # set of results so we will maintain this while filtering using another generator
+    for key in keys:
+        valid = True
+        if prefix is not None and not key.name.startswith(prefix):
+            valid = False
+        if key_filter is not None and not re.match(key_filter, key.name):
+            valid = False
+        if valid:
+            yield key
+
+
+def initiate_ppp(queue, key):
+    pass
+
+
+def get_options():
+
+    usage = "usage: %prog [options] bucket_name workflow_starter_queue"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-p", "--prefix", default=None, action="store", type="string", dest="prefix",
+                      help="only feed keys with the given prefix")
+    parser.add_option("-r", "--rate", default="30", action="store", type="int",
+                      dest="rate", help="how many seconds between messages")
+    parser.add_option("-f", "--filter", default=None, action="store", type="string", dest="filter",
+                      help="filter regex to match against keys")
+
+    opts, ags = parser.parse_args()
+    if len(ags) != 2:
+        parser.error("incorrect number of arguments")
+
+    return opts, ags
+
+
+if __name__ == "__main__":
+
+    options, args = get_options()
+
+    # args[0] = bucket_name, args[1] = queue_name
+    feed_ppp(args[0], args[1], options.rate, options.prefix, options.filter)
---
+++ readme.md
@@ -0,0 +1,22 @@
+# ppp-feeder
+
+### Configuration
+
+You must set the following environment variables before running the program (AWS_PROFILE is optiona):
+
+* AWS_ACCESS_KEY_ID The access key for your AWS account.
+* AWS_SECRET_ACCESS_KEY The secret key for your AWS account.
+* AWS_DEFAULT_REGION The default region to use, e.g. us-east-1.
+* AWS_PROFILE The default credential and configuration profile to use, if any. (optional)
+
+### Operation
+
+Usage:
+ppp-feeder.py [options] bucket_name
+
+Options:
+
+*  -h, --help  - show this help message and exit
+*  -p PREFIX, --prefix=PREFIX   - only feed keys with the given prefix
+*  -r RATE, --rate=RATE  - how many seconds between messages
+*  -f FILTER, --filter=FILTER  - filter regex to match against keys
