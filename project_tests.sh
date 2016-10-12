ad349636c74efa583a8fda38bef15fd0255f5be5
Giorgio Sironi <g.sironi@elifesciences.org>
2016-10-12 09:56:11 +0100

    Added coverage measurement for econ_article_feeder

--- project_tests.sh
+++ project_tests.sh
@@ -5,4 +5,6 @@ rm -rf venv
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
-pytest test_feeder.py
+coverage run -m pytest test_feeder.py
+coverage report -m econ_article_feeder.py
+
