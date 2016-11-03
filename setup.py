cf6da07a7e3f7b7e54335a3088eedd0075ccfbe4
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-03 17:36:49 +0000

    Attempting to use setuptools to allow installation of this package as dependency
    Main problem is the files are not in a package, so I'm not sure I can use the package as-is from another Python project (no problem in depending on the Git repo instead of PyPI). Thoughts?

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md') as fp:
+    readme = fp.read()
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(name='econtools',
+    version='0.1',
+    description='Tools for using article data in Python.',
+    long_description=readme,
+    packages=[], #'econ_article_feeder', 'econ_dashprop'],
+    license = 'MIT',
+    install_requires=install_requires,
+    url='https://github.com/elifesciences/econ-tools',
+    maintainer='eLife Sciences Publications Ltd.',
+    maintainer_email='py@elifesciences.org',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        ]
+    )
+
