584514a7f3ee15cb0bbe87bd6a88598b5566b92f
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-07 13:30:02 +0000

    Moved files into package econtools

--- project_tests.sh
+++ project_tests.sh
@@ -6,6 +6,6 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 pylint -E *.py
-coverage run -m pytest test_feeder.py
-coverage report -m econ_article_feeder.py
+coverage run -m pytest econtools/test_feeder.py
+coverage report -m econtools/*.py
 
