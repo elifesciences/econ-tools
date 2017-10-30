f7289e67a0f70e99a4b133c53cebab375ba34417
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-30 11:23:16 +0000

    Forcing Python 3 in virtualenv

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e # everything must succeed.
-if [ ! -d venv ]; then
-    virtualenv --python=`which python2` venv
+if [ ! -d venv/bin/python3 ]; then
+    rm -rf venv
+    virtualenv --python=`which python3` venv
 fi
 source venv/bin/activate
 pip install -r requirements.txt
