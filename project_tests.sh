d545964b907ecd96858fca92c85b80cd0a66111c
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-12 15:55:14 +0000

    Run all available tests

--- project_tests.sh
+++ project_tests.sh
@@ -6,6 +6,6 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 pylint -E *.py
-coverage run -m pytest econtools/test_feeder.py
+coverage run -m pytest econtools/test_*.py
 coverage report -m econtools/*.py
 
