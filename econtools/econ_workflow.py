37339eb89a3a92c43e500bca04120194dab7c16f
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-15 10:28:30 +0000

    Added workflow_data argument

--- econtools/econ_workflow.py
+++ econtools/econ_workflow.py
@@ -10,12 +10,12 @@ import boto.sqs.connection
 from econtools.aws import get_queue
 
 
-def start_workflow(queue_name, workflow_name):
+def start_workflow(queue_name, workflow_name, workflow_data={}):
 
     queue = get_queue(queue_name)
     message = {
         'workflow_name': workflow_name,
-        'workflow_data': {}
+        'workflow_data': workflow_data,
     }
 
     msg = boto.sqs.connection.Message()
@@ -27,13 +27,14 @@ def get_args():
     parser = ArgumentParser(description="Starts any workflow")
     parser.add_argument('queue_name', type=str, help='The queue to add the starting message to')
     parser.add_argument('workflow_name', type=str, help='The workflow type to start')
+    parser.add_argument('workflow_data', type=str, help='The workflow data in JSON format e.g. \`{"a": 42}\`', nargs='?', default='{}')
 
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    start_workflow(args.queue_name, args.workflow_name)
+    start_workflow(args.queue_name, args.workflow_name, json.loads(args.workflow_data))
 
 
 if __name__ == "__main__":
