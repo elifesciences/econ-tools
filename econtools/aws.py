231dd9fa129aa5c760e0615208fa1e3fdd3c11c8
Luke <lsh-0@users.noreply.github.com>
2020-09-25 16:07:11 +0930

    added a key check to get_queue for missing env var

--- econtools/aws.py
+++ econtools/aws.py
@@ -3,12 +3,16 @@ import boto.sqs.connection
 import os, sys
 
 def get_queue(queue_name):
-    sqs_conn = boto.sqs.connect_to_region(os.environ['AWS_DEFAULT_REGION'])
+    region = os.environ.get('AWS_DEFAULT_REGION')
+    if not region:
+        print("environment variable 'AWS_DEFAULT_REGION' not set")
+        sys.exit(1)
+    sqs_conn = boto.sqs.connect_to_region(region)
     if sqs_conn is None:
-        print("Cannot connect to SQS for region %s" % os.environ['AWS_DEFAULT_REGION'])
+        print("Cannot connect to SQS for region %s" % region)
         sys.exit(1)
     queue = sqs_conn.get_queue(queue_name)
     if queue is None:
         print("Could not obtain workflow starter queue %s\n" % (queue_name,))
-        sys.exit(2)
+        sys.exit(1)
     return queue
