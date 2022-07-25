ef92e5ff8fe18c97d33947dc47098702a3826e08
Luke <lsh-0@users.noreply.github.com>
2022-07-25 11:08:21 +0930

    updates tests

--- econtools/test_feeder.py
+++ econtools/test_feeder.py
@@ -10,55 +10,60 @@ class TestFeeder(unittest.TestCase):
     @patch('econtools.econ_article_feeder.initiate_econ_feed')
     @patch('time.sleep')
     def test_feed_two_articles(self, sleep, initiate_econ_feed, get_filtered_keys, get_queue):
-        queue = MagicMock()
-        get_queue.return_value = queue
+        queue_mock = MagicMock()
+        bucket_name = 'ct-elife-production-final'
+        queue_name = 'ct-workflow-starter-queue'
+        workflow_name='MyArticleWorkflow'
+        get_queue.return_value = [queue_mock, queue_name]
         get_filtered_keys.return_value = [
             'elife-12345-vor-r1.zip',
             'elife-67890-vor-r1.zip',
         ]
-        econ_article_feeder.feed_econ('ct-elife-production-final', 'ct-workflow-starter-queue', workflow_name='MyArticleWorkflow')
-        self.assertEqual(
-            initiate_econ_feed.mock_calls,
-            [
-                call(queue, 'elife-12345-vor-r1.zip', 'MyArticleWorkflow'),
-                call(queue, 'elife-67890-vor-r1.zip', 'MyArticleWorkflow'),
-            ]
-        )
-        self.assertEqual(
-            sleep.mock_calls,
-            [
-                call(30),
-                call(30)
-            ]
-        )
+
+        expected = [
+            call(queue_name, bucket_name, 'elife-12345-vor-r1.zip', workflow_name),
+            call(queue_name, bucket_name, 'elife-67890-vor-r1.zip', workflow_name)
+        ]
+        econ_article_feeder.feed_econ(bucket_name, queue_name, workflow_name='MyArticleWorkflow')
+        self.assertEqual(initiate_econ_feed.mock_calls, expected)
+
+        expected_sleeps = [
+            call(30),
+            call(30)
+        ]
+        self.assertEqual(sleep.mock_calls, expected_sleeps)
 
     @patch('econtools.aws.get_queue')
     @patch('econtools.econ_article_feeder.now')
     def test_feeding_sends_an_sqs_message(self, now, get_queue):
         now.return_value = datetime.strptime('2016-01-01', '%Y-%m-%d')
-        queue = MagicMock()
-        get_queue.return_value = queue
+        queue_mock = MagicMock()
+        queue_name = 'foo'
+        get_queue.return_value = [queue_mock, queue_name]
         bucket_name = 'ct-elife-production-final'
         key = {
             'Name': 'elife-12345-vor-r1.zip',
             'ETag': '"123"',
             'Size': 2 * 1024 * 1024
         }
-        econ_article_feeder.initiate_econ_feed(queue, bucket_name, key, 'MyArticleWorkflow')
-        self.assertEqual(len(queue.method_calls), 1)
-        (_, args, _) = queue.method_calls[0]
-        message_body = args[0].get_body()
-        self.assertEqual(
-            json.loads(message_body),
-            {
-                'workflow_name': 'MyArticleWorkflow',
-                'workflow_data': {
-                    'event_time': '2016-01-01T00:00:00Z',
-                    'event_name': 'ObjectCreated:Put',
-                    'file_name': 'elife-12345-vor-r1.zip',
-                    'file_etag': '123',
-                    'bucket_name': 'ct-elife-production-final',
-                    'file_size': 2 * 1024 * 1024,
-                },
+        workflow_name = 'MyArticleWorkflow'
+        econ_article_feeder.initiate_econ_feed(queue_name, bucket_name, key, workflow_name)
+        
+        self.assertEqual(len(queue_mock.method_calls), 1)
+
+        expected_message_body = {
+            'workflow_name': 'MyArticleWorkflow',
+            'workflow_data': {
+                'event_time': '2016-01-01T00:00:00Z',
+                'event_name': 'ObjectCreated:Put',
+                'file_name': 'elife-12345-vor-r1.zip',
+                'file_etag': '123',
+                'bucket_name': 'ct-elife-production-final',
+                'file_size': 2 * 1024 * 1024,
             }
-        )
+        }
+
+        _, _, args = queue_mock.method_calls[0]
+        message_body = args['MessageBody']
+
+        self.assertEqual(json.loads(message_body), expected_message_body)
--- econtools/test_workflow.py
+++ econtools/test_workflow.py
@@ -1,22 +1,20 @@
 import json
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, call
 from econtools import econ_workflow
 
-class TestWorkflow(unittest.TestCase):
-    @patch('econtools.econ_workflow.get_queue')
-    def test_feed_two_articles(self, get_queue):
-        queue = MagicMock()
-        get_queue.return_value = queue
-        econ_workflow.start_workflow('ct-workflow-starter-queue', workflow_name='MyArticleWorkflow')
+def test_start_workflow():
+    queue_mock = MagicMock()
+    queue_name = 'ct-workflow-starter-queue'
+    workflow_name = 'MyArticleWorkflow'
 
-        self.assertEqual(len(queue.method_calls), 1)
-        (_, args, _) = queue.method_calls[0]
-        message_body = args[0].get_body()
-        self.assertEqual(
-            json.loads(message_body),
-            {
-                'workflow_name': 'MyArticleWorkflow',
-                'workflow_data': {},
-            }
-        )
+    expected_message_body = {
+        'workflow_name': workflow_name,
+        'workflow_data': {}}
+    expected = call(**{'QueueUrl': queue_name,
+                       'MessageBody': json.dumps(expected_message_body)})
+
+    with patch('econtools.aws.get_queue', return_value=(queue_mock, queue_name)):
+        econ_workflow.start_workflow(queue_name, workflow_name)
+        assert len(queue_mock.method_calls) == 1
+        assert queue_mock.send_message.call_args_list == [expected]
